e25bddd05539cfa76f4fffd17d302332e93b3318
Ganlv <ganlvtech@qq.com>
2019-11-18 06:54:21 +0800

    Remove debug. Improve args

--- main.go
+++ main.go
@@ -1,11 +1,10 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"os"
 	"os/signal"
-	"runtime"
-	"runtime/debug"
 	"syscall"
 	"time"
 
@@ -15,8 +14,22 @@ import (
 	"github.com/ganlvtech/go-game-matching/matcher"
 )
 
+var maxTime int
+var maxScore int
+var scoreGroupLen int
+var matchCount int
+
+func init() {
+	flag.IntVar(&maxTime, "max_time", 180, "最长匹配时间，超过时间会被移出匹配队列，不超过二倍时间会保留玩家匹配信息，超过二倍时间会自动清除该角色的所有信息")
+	flag.IntVar(&maxScore, "max_score", 300, "最大分数")
+	flag.IntVar(&scoreGroupLen, "score_group_len", 10, "每一分段长度，越短匹配越精确，稍长性能会好，但是过长性能会很差")
+	flag.IntVar(&matchCount, "match_count", 25, "每组匹配人数")
+}
+
 func main() {
-	matchingServer := agent.NewHttpMatchingServer(180, 300, 10)
+	flag.Parse()
+
+	matchingServer := agent.NewHttpMatchingServer(matcher.Time(maxTime), matcher.PlayerScore(maxScore), scoreGroupLen)
 	server := fasthttp.Server{
 		Handler: matchingServer.HandleHTTP,
 		Name:    "go-game-matching",
@@ -38,7 +51,7 @@ func main() {
 	go func() {
 		log.Println("Matching service started.")
 		for isRun {
-			matchingServer.Match(matcher.Time(time.Now().Unix()), 25)
+			matchingServer.Match(matcher.Time(time.Now().Unix()), matchCount)
 			time.Sleep(time.Second)
 		}
 	}()
@@ -46,22 +59,18 @@ func main() {
 	go func() {
 		log.Println("Sweeping service started.")
 		for isRun {
-			log.Println("Before sweeping.", matchingServer.Matcher.PlayerCount())
-			matchingServer.Sweep(matcher.Time(time.Now().Unix() - 10))
-			runtime.GC()
-			debug.FreeOSMemory()
-			log.Println("After sweeping.", matchingServer.Matcher.PlayerCount())
-			time.Sleep(10 * time.Second)
+			matchingServer.Sweep(matcher.Time(int(time.Now().Unix()) - maxTime*2))
+			time.Sleep(time.Duration(maxTime) * time.Second)
 		}
 	}()
 
-	log.Println("HTTP server started.")
 	addr := ":8000"
 	if len(os.Args) >= 2 {
 		addr = os.Args[1]
 	}
+	log.Println("HTTP server listening " + addr)
 	if err := server.ListenAndServe(addr); err != nil {
 		log.Fatal(err)
 	}
-	log.Println("Server exit.")
+	log.Println("Server main exit.")
 }
